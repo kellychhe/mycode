ab60bdcad967143f0f6c66c49cc4ba804669a811
Kelly Chhe <kellykchhe@users.noreply.github.com>
2022-09-21 17:43:20 +0000

    copying local files with python

---
+++ copycat01.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Using shutil and os to move and duplicate files and directories"""
+
+# import additional code to complete our task
+import shutil
+import os
+
+def main():
+
+    # move into the working directory
+    os.chdir("/home/student/mycode/")
+
+    # copy the fileA to fileB
+    shutil.copy("5g_research/sdn_network.txt", "5g_research/sdn_network.txt.copy")
+
+    # copy the entire directoryA to directoryB
+    os.system("rm -rf /home/student/mycode/5g_research_backup/")
+    shutil.copytree("5g_research/", "5g_research_backup/")
+
+# run the function
+if __name__ == "__main__":
+    main()
